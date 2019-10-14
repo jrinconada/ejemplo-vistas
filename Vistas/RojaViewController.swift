00d20bba3373652498875108007d07fba6ac95ee
Juan <juanrinconada@cev.com>
2019-10-14 09:46:11 +0200

    Initial version

---
+++ Vistas/RojaViewController.swift
@@ -0,0 +1,11 @@
+
+
+import UIKit
+
+class RojaViewController: UIViewController {
+    
+    
+    @IBAction func atras(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Vistas/ViewController.swift
@@ -0,0 +1,27 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var message: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Momento de incialización
+        print("Pantalla cargada")
+        message.text = "Mensajeeeee"
+    }
+
+    @IBAction func botonPulsado(_ sender: Any) {
+        print("botón pulsado")
+        message.text = message.text! + "pulsadoooo"
+        message.textColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+    }
+    
+    
+    @IBAction func textoCambiado(_ sender: UITextField) {
+        print(sender.text!)
+    }
+    
+}
+
